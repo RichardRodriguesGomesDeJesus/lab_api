368a53c1781bac9204a763fa9bc6ec41ef43c60a
RichardRodriguesGomesDeJesus <richard.gj.rodrigues@gmail.com>
2025-11-26 20:54:02 -0300

    refactor: ajustar formatação e estilo do código no controlador de usuários

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,78 +1,84 @@
 import { Request, Response } from "express";
-import User from "../models/user.model";
-import userRepository from "../repositories/user.repository";
+import User from "../models/user.model.ts";
+import userRepository from "../repositories/user.repository.ts";
 
 function getUserByUsername(req: Request, res: Response) {
-    const username = String(req.params.username);
+  const username = String(req.params.username);
 
-    const user = userRepository.getUserByUsername(username);
+  const user = userRepository.getUserByUsername(username);
 
-    if (!user){
-        res.sendStatus(404);
-        return;
-    }
+  if (!user) {
+    res.sendStatus(404);
+    return;
+  }
 
-    res.status(200).json(user);
+  res.status(200).json(user);
 }
 
 function getUsers(req: Request, res: Response) {
-    const users = userRepository.getUsers();
+  const users = userRepository.getUsers();
 
-    res.status(200).json(users);
+  res.status(200).json(users);
 }
 
 function getUser(req: Request, res: Response) {
-    const id = req.params.id;
-    const user = userRepository.getUserById();
+  const id = req.params.id;
+  const user = userRepository.getUserById();
 
-    if (!user) {
-        res.status(404).json({ error : "User not found" });
-    }
-    else {
-        res.status(200).json(user);
-    }
+  if (!user) {
+    res.status(404).json({ error: "User not found" });
+  } else {
+    res.status(200).json(user);
+  }
 }
 
 function createUser(req: Request, res: Response) {
-    const data = req.body as User;
-    const user = userRepository.createUser(data.username, data.password, 
-        data.userType, new Date().toISOString().split("T")[0], data.active);
+  const data = req.body as User;
+  const user = userRepository.createUser(
+    data.username,
+    data.password,
+    data.userType,
+    new Date().toISOString().split("T")[0],
+    data.active
+  );
 
-    res.status(200).json(user);
+  res.status(200).json(user);
 }
 
 function updateUser(req: Request, res: Response) {
-    const id = req.params.id;
-    const updatedUser = req.body as User
-    const user = userRepository.updateUserById(updatedUser.username, updatedUser.password,
-        updatedUser.userType, updatedUser.active, id)
+  const id = req.params.id;
+  const updatedUser = req.body as User;
+  const user = userRepository.updateUserById(
+    updatedUser.username,
+    updatedUser.password,
+    updatedUser.userType,
+    updatedUser.active,
+    id
+  );
 
-    if (!user) {
-        res.status(404).json({ error : "User not found" });
-    }
-    else {
-        res.status(200).json(user);
-    }
+  if (!user) {
+    res.status(404).json({ error: "User not found" });
+  } else {
+    res.status(200).json(user);
+  }
 }
 
-function deleteUser(req : Request, res : Response) {
-    const id = req.params.id;
-    const deletedUser = userRepository.deleteUserById(id);
+function deleteUser(req: Request, res: Response) {
+  const id = req.params.id;
+  const deletedUser = userRepository.deleteUserById(id);
 
-    if (!deletedUser) {
-        res.status(404).json({ error : "User not found" });
-    }
-    else {
-        res.status(200).json({ message : "User deleted" });
-    }
+  if (!deletedUser) {
+    res.status(404).json({ error: "User not found" });
+  } else {
+    res.status(200).json({ message: "User deleted" });
+  }
 }
 
 export default {
-    getUserByUsername,
-    getUser,
-    getUsers,
-    createUser,
-    updateUser,
-    deleteUser
-}
-
+  getUserByUsername,
+  getUser,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+};
