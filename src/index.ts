c7f92a3c913d967c7d4e5629392fd6e03310af8a
RichardRodriguesGomesDeJesus <richard.gj.rodrigues@gmail.com>
2025-11-26 22:45:24 -0300

    fix: corrigir importações de arquivos para incluir a extensão .ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import users from "./routes/user.router"
+import users from "./routes/user.router.ts";
+import "dotenv";
 
+const port = process.env.PORTNUMBER;
 const app = express();
 app.use(express.json());
 
 app.use("/api/users", users);
 
-app.listen(3000);
+app.listen(port);
+
+console.log("Running");
